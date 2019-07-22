6f87da5df57faeb6db99762bfc61bd99b36daaf5
Nguyentu1996 <50936704+Nguyentu1996@users.noreply.github.com>
2019-07-22 10:16:53 +0700

    ok

---
+++ ThucHanh/TrienKhaiInterFaceComparator/src/CircleComparatorTest.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        Circle[] circles=new Circle[2];
+        circles[1]=new Circle();
+        circles[0]= new Circle("yellow",true,3.6);
+        System.out.println("pre-sorted");
+        for (Circle circle :circles){
+            System.out.println(circle);
+        }
+        Comparator circleComparator =new CircleComparator();
+        Arrays.sort(circles,circleComparator);
+        System.out.println("After-sorted:");
+        for (Circle circle : circles){
+            System.out.println(circle);
+        }
+    }
+}
